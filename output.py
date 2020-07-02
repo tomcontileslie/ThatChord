f5c7e95c8ea139615c7f494ce2e5932c5052cee6
Tom Conti-Leslie <tom.contileslie@gmail.com>
2020-07-02 21:12:39 +0200

    Output: add "img" option which exports to PNG
    (Uses Pillow)

--- output.py
+++ output.py
@@ -91,3 +91,122 @@ def text(frets, height = 5, margin = 3, head = "=", string = "|", press = "O", m
         
     
     return out
+
+
+def img(frets, title = "", top = False, height = 5):
+    """
+    CREATES A SMALL PNG IMAGE (each pixel is one bit).
+    
+    frets is the list of pressed frets. 0 for nothing, -1 for mute.
+    
+    if a title is supplied, it is printed below the grid unless "top"
+    is true, in which case more space is made above the grid.
+    
+    height is the minimum hight of the grid.
+    
+    The size of each square in the grid is 20 pixels.
+    """
+    from PIL import Image, ImageDraw
+    
+    n = len(frets)
+    
+    # first, determine whether to print the header.
+    # lo is the lowest fret printed.
+    if max(frets) <= 5 or min([i for i in frets if i != 0 and i != -1]) < 3:
+        header = True
+        lo     = 1
+    else:
+        header = False
+        lo     = min([i for i in frets if i != 0 and i != -1])
+    
+    # h is the number of frets printed.
+    h = max(lo + height - 1, max(frets)) - lo + 1
+    
+    # determine extra space
+    if top:
+        b = 10
+    else:
+        b = 0
+    
+    # dimensions of output
+    wid = (n + 1) * 20
+    hgh = (h + 2) * 20 + b
+    
+    # create new image, white bg
+    img = Image.new("1", (wid, hgh), color = "white")
+    drw = ImageDraw.Draw(img)
+    
+    # draw header if needed
+    if header:
+        h1 = (20, 19 + b)
+        h2 = (n * 20, 19 + b)
+        drw.line([h1, h2], width = 3)
+    # otherwise, need a fret number
+    else:
+        k = len(str(lo))
+        fx = 12 - 5 * k
+        fy = 25 + b
+        drw.text((fx, fy), str(lo))
+    
+    # Draw vertical lines
+    for i in range(n):
+        l1 = ((i + 1) * 20, 20 + b)
+        l2 = ((i + 1) * 20, (h + 1) * 20 + b)
+        drw.line([l1, l2], width = 1)
+
+    # Draw horizontal lines
+    for i in range(h + 1):
+        l1 = (20, (i + 1) * 20 + b)
+        l2 = (n * 20, (i + 1) * 20 + b)
+        drw.line([l1, l2], width = 1)
+
+    # make a function for circles and crosses
+    def mark(string, fret, r = 5):
+        """
+        r is the radius.
+        string numbering starts at 0.
+        fret numbering starts at 1 and -1 is also okay.
+        xc, yc are coordinates of centre of mark
+        """
+        # if no fret is pressed, then do nothing.
+        if fret == 0:
+            return
+        
+        xc = (string + 1) * 20
+        if fret > 0:
+            yc = fret * 20 + 10 + b
+            drw.ellipse([(xc - r, yc - r), (xc + r, yc + r)], fill = "black")
+        else:
+            yc = 11 + b
+            drw.line([(xc - 4, yc - 4), (xc + 3, yc + 3)], width = 2)
+            drw.line([(xc - 3, yc + 3), (xc + 4, yc - 4)], width = 2)
+    
+    # add a title if supplied
+    if title:
+        if top:
+            tx = 20
+            ty = 3
+
+        else:
+            tx = 20
+            ty = (h + 1) * 20 + 3 + b
+        drw.text((tx, ty), title)
+    
+    # the one thing we need to do before adding marks is shift everything
+    # if there is no header.
+    if not header:
+        fretsn = []
+        for note in frets:
+            if not (note == 0 or note == -1):
+                fretsn.append(note - lo + 1)
+            else:
+                fretsn.append(note)
+    else:
+        fretsn = frets
+    
+    # now add marks
+    for i in range(n):
+        mark(i, fretsn[i])
+    
+    # TODO change output depending on settings file
+    img.show()
