e87b2b2b3dd5eefe702ecc3fefd63f9054d2dd34
Tom Conti-Leslie <tom.contileslie@gmail.com>
2020-07-31 12:25:58 +0200

    Initial commit for testing

---
+++ tst/test_main.py
@@ -0,0 +1,20 @@
+###############################################################################
+###############################################################################
+##                                                                           ##
+##  THATCHORD BY TOM CONTI-LESLIE                            MAIN TEST FILE  ##
+##                                                                           ##
+##  This file contains tests to be run via pytest that check that various    ##
+##  subprocesses of ThatChord are running correctly. Some tests check        ##
+##  assertions that do not necessarily have to be true for the code to be    ##
+##  working: for example, one test ensures that the ukulele ranking          ##
+##  system places the [0, 0, 0, 3] chord for C first.                        ##
+##                                                                           ##
+##                                                                           ##
+##  License: CC BY-SA 4.0                                                    ##
+##                                                                           ##
+##  Contact: tom (dot) contileslie (at) gmail (dot) com                      ##
+##                                                                           ##
+###############################################################################
+###############################################################################
+
+
