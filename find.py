5efba4a353d4d6a01a37229587cb7fcafd348632
Tom Conti-Leslie <tom.contileslie@gmail.com>
2020-08-28 23:46:36 +0100

    Faster: complete smart incrementation
    and other minor speedups

--- find.py
+++ find.py
@@ -22,27 +22,11 @@ from errors import err
 # import ranking functions
 import rank
 
-def increment(maxes, current):
-    """
-    Helper function for incrementing a list of indices, 'current', by
-    increasing the first index if possible, and if not, the next one, etc.
-    (analogous to little endian)
-    
-    The max value for each position is that value in the list 'maxes'.
-    
-    N.B. modifies input 'current'.
-    """
-    for i in range(len(maxes)):
-        if not current[i] == maxes[i] - 1:
-            current[i] += 1
-            return
-        current[i] = 0
-
-def smart_increment(maxes, current, chordset, stringset):
-    # Our current attempt is current. How many notes does that attempt play?
-    # How many notes does our chord need? If there is a gap of k notes between
-    # the two, then we need to change at least the first k counters.
-    k = len(chordset) - len(stringset)
+def smart_increment(maxes, current, remaining):
+    # How many notes is our chord missing? I.e. how big is the remaining set?
+    # If there is a gap of k notes, then we need to change at least the
+    # first k counters.
+    k = len(remaining)
     # if n is 0 or 1 then we don't need to skip anything; just increment
     # normally.
     k = max(k - 1, 0)
@@ -54,9 +38,8 @@ def smart_increment(maxes, current, chordset, stringset):
             current[i] += 1
             break
         current[i] = 0
-    # the counter i will now be set at the number of the rightmost string
-    # changed. Return this to the main loop to update the chordset.
-    return i
+    # the counter i will now be set to the number of strings changed, minus 1
+    return i + 1
             
 
 def insert(options, frets, index, r):
@@ -66,7 +49,7 @@ def insert(options, frets, index, r):
     current options. Pushes the worse options out of the list.
     r is the calculated rank of the option 'frets'.
     """
-    tup = (frets, r)
+    tup = (frets.copy(), r)
     # start at worse end of list, move all the way to where r is in order
     l = len(options)
     i = l
@@ -107,12 +90,19 @@ def find(chord, nmute = 0, important = 0, index = 1, nfrets = 12,
     important, if nonzero, is the number of notes from the requested chord that
     suffice to "define" the chord.
     """
+    # start by cutting off the chord so that we don't have more distinct notes
+    # than strings to play!
+    if len(chord) > len(tuning):
+        chord = chord[:len(tuning)]
     # define some basic variables. If 0 important notes, we assume the whole
     # chord is needed.
     n = len(tuning)
     valids = []
     if important == 0:
         important = len(chord)
+    # if there are more important notes than strings, cutoff at len(tuning)
+    important = min(important, len(tuning))
+    # create the set of notes we absolutely need in the chord
     chordset = set(chord[:important])
     
     # start by finding all valid positions.
@@ -137,19 +127,31 @@ def find(chord, nmute = 0, important = 0, index = 1, nfrets = 12,
     current = [0] * n
     options = []
     
+    # initiate our first attempt as the lowest value on all strings.
+    attempt = [valids[i][0] for i in range(n)]
+
     # populate list of note multiplicities. mults[i] is equal to the number of
     # distinct strings playing i.
     mults = [0] * 12
     for i in range(n):
-        pass
+        mults[(tuning[i] + attempt[i]) % 12] += 1
+    # create set of remaining notes: notes that are not played in the current
+    # attempt but needed in the chord.
+    remaining = set()
+    for note in chordset:
+        if mults[note] == 0:
+            remaining.add(note)
     
     # want to iterate until we see 000..0 again
     first_value = [0] * n
     first_time  = True
     
+    # FOR TESTING PURPOSES - function attribute
+    find.count = 0
+    
     while first_time or current != first_value:
         first_time = False
-        attempt = [valids[s][current[s]] for s in range(n)]
+        # attempt = [valids[s][current[s]] for s in range(n)]
         
         # we will assess whether mutes are valid, and then whether sufficiently
         # many notes from the required chord have been hit.
@@ -161,18 +163,46 @@ def find(chord, nmute = 0, important = 0, index = 1, nfrets = 12,
             if not len(muted) == max(muted) + 1:
                 bool_mute = False
         
-        # Second, check that the attempt covers the important notes
-        notes = [(attempt[i] + tuning[i]) % 12 for i in range(n) if attempt[i] != -1]
-        bool_impo = len(set(chord[:important]) - set(notes)) == 0
+        # Second, check that the attempt covers the important notes.
+        # This is the case iff our remaining set is empty.
+        bool_impo = len(remaining) == 0
         
         # if both conditions are satisfied, store the option.
         if bool_mute and bool_impo:
             r = rank.rank(attempt, chord, tuning, order, ranks, stringstarts)
             insert(options, attempt, index, r)
+            find.count += 1
         
-        # increment the current attempt to the next possibility.
-        increment(maxes, current)
+        # intelligently increment the current attempt to the next possibility.
+        updated = smart_increment(maxes, current, remaining)
+        # we may have changed the values of several strings.
+        # smart_increment has told us how many, and we can update the
+        # remaining set by removing notes that are no longer played now that
+        # we have changed our fret positions.
+        for i in range(updated):
+            # only remove a note if it was not muted.
+            if attempt[i] != -1:
+                old_note = (attempt[i] + tuning[i]) % 12
+                mults[old_note] -= 1
+                if mults[old_note] == 0:
+                    # we have lost all copies of this old note. It is now remaining
+                    remaining.add(old_note)
+            # update that entry of the attempt to the new value
+            attempt[i] = valids[i][current[i]]
+        # now calculate the new notes that are being played as a result of
+        # the incrementation
+        for i in range(updated):
+            # only add notes if the string is not muted
+            if attempt[i] != -1:
+                new_note = (attempt[i] + tuning[i]) % 12
+                mults[new_note] += 1
+                if mults[new_note] == 1:
+                    # then we have just re-introduced this note into our set
+                    # of currently played notes. It is no longer remaining.
+                    remaining.remove(new_note)
     
     # return the worst option in the list of options, which is at the index
     # requested (default is for options to have 1 entry).
+    if options == []:
+        err(16)
     return options[-1][0]
